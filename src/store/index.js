2528431241b23dfcdfda219bf23cad6cc102f5c3
mahmoud <khairymahmoud795@gmail.com>
2022-02-14 16:17:18 +0200

    RUN Sweet Alert on Added Task

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import {
     createStore
 } from "vuex";
 
+import Swal from 'sweetalert2'
+
+
 export default createStore({
     state: {
         item: {
@@ -23,6 +26,12 @@ export default createStore({
                     state.vaildate_Add.status = false
                     state.vaildate_Add.class_Added = ''
                     state.item.task = ''
+                    Swal.fire({
+                        title: 'Greet Job!',
+                        text: 'You Added New Task',
+                        icon: "success",
+                        timer: 2000
+                    })
                 }
             }else{
                 state.vaildate_Add.status = true
